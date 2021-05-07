8a274b4cee8408aa4713591775b9f5ef6d89f1e9
Ian Clark <idclark13@gmail.com>
2021-05-07 11:50:50 -0700

    refactor print to logging

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -7,12 +7,22 @@ import (
 	"rest-api/internal/comment"
 	"rest-api/internal/database"
 	transport "rest-api/internal/transport/http"
+
+	log "github.com/sirupsen/logrus"
 )
 
-type App struct{}
+type App struct {
+	Name    string
+	Version string
+}
 
 func (app *App) Run() error {
-	fmt.Println("Setting up my app")
+	log.SetFormatter(&log.JSONFormatter{})
+	log.WithFields(
+		log.Fields{
+			"AppName":    app.Name,
+			"AppVersion": app.Version,
+		}).Info("Setting Up Our App")
 
 	var err error
 	db, err := database.NewDatabase()
@@ -23,6 +33,7 @@ func (app *App) Run() error {
 	err = database.MigrateDB(db)
 
 	if err != nil {
+		log.Error("failed to setup database")
 		return err
 	}
 
@@ -32,6 +43,7 @@ func (app *App) Run() error {
 	handler.SetupRoutes()
 
 	if err := http.ListenAndServe(":8080", handler.Router); err != nil {
+		log.Error("Failed to setup server")
 		return err
 
 	}
@@ -42,10 +54,14 @@ func (app *App) Run() error {
 
 func main() {
 	fmt.Println("Go Rest API!")
-	app := App{}
+	app := App{
+		Name:    "Comment Service",
+		Version: "1.0.0",
+	}
 
 	if err := app.Run(); err != nil {
-		fmt.Println("=== Error Starting Application ===")
-		fmt.Println(err)
+		log.Error(err)
+		log.Fatal("Error starting up API")
+
 	}
 }
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/jackc/pgproto3/v2 v2.0.7 // indirect
 	github.com/jackc/pgx/v4 v4.11.0 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
 	github.com/stretchr/testify v1.7.0 // indirect
 	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
 	golang.org/x/text v0.3.6 // indirect
